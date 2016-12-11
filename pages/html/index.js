412e3d5200ef1ee6c00de0cd290cc3f40a9f89df
suinia <suini_a@163.com>
2016-12-11 14:37:03 +0800

    图片适配

--- pages/html/index.js
+++ pages/html/index.js
@@ -10,7 +10,7 @@ Page({
         var url = e.currentTarget.dataset.src;
         wx.showModal({
             title: '这是一个外部链接',
-            content: '链接地址: '+url
+            content: '链接地址: ' + url
         })
     },
     htmlParseImageTab: function (e) {//富文图片点击预览
@@ -24,22 +24,36 @@ Page({
             urls: urls
         });
     },
-    htmlParseImageLoad: function(e){//富文图片满屏适配
+	htmlParseImageLoad: function(e){//富文图片满屏适配
 		var id = e.currentTarget.dataset.src,
 			img_w = e.detail.width,
 			img_h = e.detail.height,
 			ratio = 690/img_w;
-		var imageStyle = 'width: 690rpx; height:'+ img_h*ratio +'rpx;';
+		if(img_w/this.dp>=690){
+			var imageStyle = 'width: 690rpx; height:'+ img_h*ratio +'rpx;';
+		}else{
+			var imageStyle = 'width: '+img_w+'px; height:'+ img_h +'px;';
+		}
 		var htmlParseImageStyle = this.data.htmlParseImageStyle;
 		htmlParseImageStyle[id] = imageStyle;
 		this.setData({
 			htmlParseImageStyle: htmlParseImageStyle
 		});
 	},
+    htmlEscape: function (html) {
+        return html.replace(/&nbsp;/g, ' ').replace(/&quot;/g, '"').replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>')
+    },
     onLoad: function (options) {
+        var winWidth = null, dp = 1;
+		try {var winWidth = wx.getSystemInfoSync().windowWidth;
+		} catch (e) { };
+		if(winWidth){
+			dp = winWidth/750;
+		}
+        this.dp = dp;
         var type = options.type,
             self = this;
-        if(type == 'json'){
+        if (type == 'json') {
             request.get(apiUrls.getTopic, {
                 id: '1'
             }).then(function (res) {
@@ -49,12 +63,13 @@ Page({
             });
             return false;
         }
-        if(type == 'html'){
+        if (type == 'html') {
             request.get(apiUrls.getHtmlTopic, {
                 id: '1'
             }).then(function (res) {
-                res.json_content = res.json_content.replace(/\\n/g,'\n');
-                res.json_content = html2json(res.json_content);
+                res.json_content = res.json_content.replace(/\\n/g, '\n');
+                res.json_content = JSON.stringify(html2json(res.json_content));
+                res.json_content = JSON.parse(self.htmlEscape(res.json_content));
                 self.setData({
                     topic: res,
                 })
